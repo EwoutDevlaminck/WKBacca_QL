f7a79cf7a40ce6f8f10ad7c3d3e222a61e545e4f
Ewout Devlaminck <ewout.devlaminck@gmail.com>
2024-08-29 18:44:57 +0200

    Bullshit work but it seems to be fine, continue.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from QL_bounce_calc_v2 import *
+from QL_bounce_calc_v3 import *
 
 import warnings
 warnings.filterwarnings("ignore") # Fuck around until I find out
@@ -8,7 +8,7 @@ ksi = np.linspace(-.999, .999, 30)
 # We need a binning of the data according to the original liuqe/LUKE grids!
 # For now the liuqe grid, but later ideally we'll have the LUKE one
 # Look into this!
-filename_WKBeam = '/home/devlamin/Documents/WKBeam_related/WKBacca_dev_v1/WKBacca_cases/TCV72644/t_1.05/output/L1_binned_QL.hdf5'
+filename_WKBeam = '/home/devlamin/Documents/WKBeam_related/WKBacca_dev_v1/WKBacca_cases/TCV72644/t_1.05/Output_nofluct/L1_binned_QL.hdf5'
 WhatToResolve, FreqGHz, mode, Wfct, Absorption, EnergyFlux, rho, theta, Npar, Nperp = read_h5file(filename_WKBeam)
 
 filename_Eq = '/home/devlamin/Documents/WKBeam_related/WKBacca_QL/WKBacca_cases/TCV72644/t_1.05/L1_raytracing.txt'
@@ -17,7 +17,7 @@ idata = InputData(filename_Eq)
 Eq = TokamakEquilibrium(idata)
 
 DRF0_wh, DRF0D_wh, DRF0F_wh, DRF0_hw, DRF0D_hw, DRF0F_hw, DRF0_hh, DRF0D_hh = \
-    D_RF(rho**2, theta, p_norm, ksi, Npar, Nperp, Wfct, Eq, n=[2, 3], FreqGHz=FreqGHz)
+    D_RF(rho**2, theta, p_norm, ksi, Npar, Nperp, Wfct, Eq, n=[2], FreqGHz=FreqGHz)
 
 
 Pw, Kh = np.meshgrid(p_norm, ksi[:-1])
